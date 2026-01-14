e454867e6cb90954a1a588f2010013e1b89e907b
Renan <renanmerlotti@gmail.com>
2026-01-13 23:44:11 -0300

    Criacao e configuracao da REST API no EmployeeController para testar com Postman e dando inicio a criacao do EmployeeRepository

---
+++ src/main/java/com/project/ems_backend/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package com.project.ems_backend.controller;
+
+import com.project.ems_backend.dto.EmployeeDto;
+import com.project.ems_backend.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    //REST API Add Employee
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
+        EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+}
