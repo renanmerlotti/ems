aa4a05fb03b107963da8956ff5eb93c19f7b4303
Renan <renanmerlotti@gmail.com>
2026-01-14 21:19:59 -0300

    Criação do primeiro tipo de exceção Criação dos metodos getEmployeeById e getAllEmployees e configurando ambos na Rest API

--- src/main/java/com/project/ems_backend/controller/EmployeeController.java
+++ src/main/java/com/project/ems_backend/controller/EmployeeController.java
@@ -5,10 +5,9 @@ import com.project.ems_backend.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -25,4 +24,18 @@ public class EmployeeController {
         return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
 
+    //REST API Get Employee
+    @GetMapping("{id}")
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long employeeId) {
+        EmployeeDto employeeDto = employeeService.getEmployeeById(employeeId);
+
+        return ResponseEntity.ok(employeeDto);
+    }
+
+    //REST API Get All Employees
+    @GetMapping
+    public ResponseEntity<List<EmployeeDto>> getAllEmployees() {
+        List<EmployeeDto> employees = employeeService.getAllEmployees();
+        return ResponseEntity.ok(employees);
+    }
 }
--- src/main/java/com/project/ems_backend/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/project/ems_backend/service/impl/EmployeeServiceImpl.java
@@ -2,11 +2,15 @@ package com.project.ems_backend.service.impl;
 
 import com.project.ems_backend.dto.EmployeeDto;
 import com.project.ems_backend.entity.Employee;
+import com.project.ems_backend.exception.ResourceNotFoundException;
 import com.project.ems_backend.mapper.EmployeeMapper;
 import com.project.ems_backend.repository.EmployeeRepository;
 import com.project.ems_backend.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -21,5 +25,23 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee savedEmployee = employeeRepository.save(employee);
 
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("O funcionário não foi encontrado com o id: " + employeeId));
+
+
+        return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+
+        return employees.stream().map((employee) -> EmployeeMapper.mapToEmployeeDto(employee))
+                .collect(Collectors.toList());
     }
 }
